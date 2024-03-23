d22d019dde2ef2898acf1a847f72da23e52f2bce
kaanhakangul <kaanhakangul@gmail.com>
2024-03-23 22:29:29 +0300

    US_405_Elements and US_405_POM are created

---
+++ src/US_405/US_405_Elements.java
@@ -0,0 +1,6 @@
+package US_405;
+
+public class US_405_Elements {
+
+    //Locaterları tanımlayacağımız kısım
+}
---
+++ src/US_405/US_405_POM.java
@@ -0,0 +1,6 @@
+package US_405;
+
+public class US_405_POM {
+
+    //Kodların olacağı kısım
+}
